5a305f46d01a0b8673c66040cb3128c72c705117
Tate Wilhelm <t8wilhelm@gmail.com>
2023-09-04 09:04:44 -0600

    Added searc

---
+++ themes/custom-theme/static/js/filter.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const projects = document.querySelectorAll('.project-box');
+    const filterClassic = document.getElementById('filterClassic');
+    const filterCX = document.getElementById('filterCX');
+    const searchBar = document.getElementById('searchBar');
+
+    function filterProjects() {
+        projects.forEach(project => {
+            const title = project.querySelector('.project-title').textContent.toLowerCase();
+            const isClassic = project.querySelector('.available') && project.querySelector('.available').textContent.includes('Classic');
+            const isCX = project.querySelector('.available') && project.querySelector('.available').textContent.includes('CX / CX II');
+            const searchTerm = searchBar.value.toLowerCase();
+
+            let display = true;
+
+            if (filterClassic.checked && !isClassic) display = false;
+            if (filterCX.checked && !isCX) display = false;
+            if (searchTerm && !title.includes(searchTerm)) display = false;
+
+            project.style.display = display ? 'flex' : 'none';
+        });
+    }
+
+    filterClassic.addEventListener('change', filterProjects);
+    filterCX.addEventListener('change', filterProjects);
+    searchBar.addEventListener('input', filterProjects);
+});
